d59549a3a02e1c8ba39c7cc0c55f05a519372e84
Izadora Braga <izadora_abraga@outlook.com>
2024-03-05 17:12:10 -0300

    requisito 2

--- src/talkerRoutes.js
+++ src/talkerRoutes.js
@@ -13,4 +13,20 @@ router.get('/', async (_request, response) => {
   }
 });
 
+router.get('/:id', async (request, response) => {
+  try {
+    const data = await fs.readFile('./src/talker.json', 'utf8');
+    const talkerData = JSON.parse(data);
+    const talker = talkerData.find((person) => person.id === parseInt(request.params.id, 10));
+
+    if (!talker) {
+      return response.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+
+    response.status(200).json(talker);
+  } catch (error) {
+    console.error(`Erro ao ler o arquivo 'talker.json': ${error.message}`);
+  }
+});
+
 module.exports = router;
